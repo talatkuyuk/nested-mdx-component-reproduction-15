71fe2980a73045a9844661bfb6fe29bc439febb7
talatkuyuk <talatkuyuk@gmail.com>
2025-01-31 15:11:15 +0300

    added global JSX namespace for fixing the type error related with nested MDX components

--- components/MDXPage.tsx
+++ components/MDXPage.tsx
@@ -3,6 +3,13 @@ import { evaluate } from "@mdx-js/mdx";
 import * as runtime from "react/jsx-runtime";
 import type { MDXComponents } from "mdx/types";
 
+import type { JSX as ReactJSX } from "react";
+declare global {
+  namespace JSX {
+    type IntrinsicElements = ReactJSX.IntrinsicElements;
+  }
+}
+
 export default async function Home() {
   const { default: MDXContent } = await evaluate("<motion.p />", runtime);
 
